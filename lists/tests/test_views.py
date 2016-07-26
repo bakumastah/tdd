b2dd7f5dc4015cfe3ae870f6cafddc9dce93471b
Jakub Bąk <jakub.bak@dcx.bz>
2016-07-26 15:51:37 +0200

    Add duplicate items validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,4 @@
+from unittest import skip
 from django.test import TestCase
 from django.utils.html import escape
 
@@ -99,6 +100,20 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/{:d}/'.format(list_.id))
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+
+    @skip('')
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/{:d}/'.format(list1.id,),
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape('You\'ve already got this in your list')
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'lists/list.html')
+        self.assertEqual(Item.objects.count(), 1)
 
 
 class NewListTest(TestCase):
